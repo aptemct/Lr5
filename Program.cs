c1b3b2ff1458af2443f1ea65eda7258eb0564446
aptemct <120793748+aptemct@users.noreply.github.com>
2022-12-19 21:18:16 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PI
+{
+    public class gruz
+    {
+        public readonly string type;
+        public readonly string track;
+        public readonly int cost;
+        public readonly DateTime data_otgr;
+        public readonly DateTime data_vozvr;
+        public readonly int koef_skidki;
+        public readonly int koef_pov;
+        public gruz(string type, string track, int cost, DateTime data_otgr, DateTime data_vozvr, int koef_skidki, int koef_pov)
+        {
+            this.type = type;
+            this.track = track;
+            this.cost = cost;
+            this.data_otgr = data_otgr;
+            this.data_vozvr = data_vozvr;
+            this.koef_skidki = koef_skidki;
+            this.koef_pov = koef_pov;
+        }
+    }
+    class gruz_list
+    {
+        private gruz[] mas;
+        public gruz_list(int n)
+        {
+            Console.Write("кол-во заказов: ");
+            n=Convert.ToInt32(Console.ReadLine());
+            mas = new gruz[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Вид груза: ");
+                string type = Convert.ToString(Console.ReadLine());
+                Console.Write("Тип перевозки: ");
+                string track = Convert.ToString(Console.ReadLine());
+                Console.Write("Базовая стоимость перевозки: ");
+                int cost = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Дата отгрузки: ");
+                DateTime data_otgr = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Дата доставки заказа: ");
+                DateTime data_vozvr = Convert.ToDateTime(Console.ReadLine());
+                Console.Write("Коэфициент зимней скидки: ");
+                int koef_skidki = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Коэффициент повышения стоимости перевозки летом: ");
+                int koef_pov = Convert.ToInt32(Console.ReadLine());
+                mas[i] = new gruz(type, track, cost, data_otgr, data_vozvr, koef_skidki, koef_pov);
+            }
+        }
+        public void output()
+        {
+            Console.WriteLine("Вид груза Тип перевозки  Cтоим п-и Дата отгр. Дата дост. Коэф.з.с. Коэф.п.с.");
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(mas[i].type + "           " + mas[i].track + "         " + mas[i].cost + "      " + mas[i].data_otgr.ToString("dd.MM.yyyy") + "  " + mas[i].data_vozvr.ToString("dd.MM.yyyy") + "  " + mas[i].koef_skidki + "        " + mas[i].koef_pov);
+            }
+        }
+        public void task1()
+        {
+            int k = 0;
+            for (int i = 0; i < 1; i++)
+                if (mas[i].data_otgr.Year == DateTime.Now.Year - 1 && mas[i].data_otgr.Month < 7)
+                {
+                    Console.WriteLine("Тип перевозки, который использовался в первом полугодии прошлого года: " + mas[i].type);
+                    k++;
+                }
+            if (k == 0) { Console.WriteLine("Ни один из типов перевозки не использовался в первом полугодии прошлого года"); }
+        }
+        public double task3()
+        {
+            double sum = 0;
+            for (int i = 0; i < 1; i++)
+            {
+                int s = mas[i].cost;
+                if ((mas[i].data_otgr.Month == 1) || (mas[i].data_otgr.Month == 2) || (mas[i].data_otgr.Month == 12))
+                {
+                    s = s - (mas[i].koef_skidki);
+                }
+                if ((mas[i].data_otgr.Month == 6) || (mas[i].data_otgr.Month == 7) || (mas[i].data_otgr.Month == 8))
+                {
+                    s = s + (mas[i].koef_pov);
+                }
+                sum += s;
+            }
+            return sum;
+        }
+        public double task2()
+        {
+            Console.WriteLine("Введите вид груза: ");
+            string str = Convert.ToString(Console.ReadLine());
+            int sum = 0;
+            int k = 0;
+            double sr_sum = 0;
+            for (int i = 0; i < 1; i++)
+            {
+                if (mas[i].type == str)
+                {
+                    int s = mas[i].cost;
+                    if ((mas[i].data_otgr.Month == 1) || (mas[i].data_otgr.Month == 2) || (mas[i].data_otgr.Month == 12))
+                    {
+                        s = s - mas[i].koef_skidki;
+                    }
+                    if ((mas[i].data_otgr.Month == 6) || (mas[i].data_otgr.Month == 7) || (mas[i].data_otgr.Month == 8))
+                    {
+                        s = s + mas[i].koef_pov;
+                    }
+                    sum += s;
+                    k++;
+                }
+            }
+            if (k == 0) { Console.WriteLine("Груз такого вида не найден"); }
+            else
+            {
+                sr_sum = sum / k;
+            }
+            return sr_sum;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                gruz_list gl = new gruz_list(5);
+                gl.output();
+                gl.task1();
+                Console.WriteLine("Cредняя стоимости перевозки груза данного вида: ");
+                Console.WriteLine(gl.task2().ToString());
+                Console.WriteLine("Общяя стоимость перевозок: ");
+                Console.WriteLine(gl.task3().ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Transport.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class Sides
+    {
+        public int[] sides;
+        public Sides()
+        {
+            sides = new int[] { 0, 0, 0, 0 };
+        }
+        public int this[int index]
+        {
+
+            get { return sides[index]; }
+            set
+            {
+                if (value <= 0) throw new ArgumentException();
+                sides[index] = value;
+                if ((sides[0] > 0) && (sides[1] > 0) && (sides[2] > 0) && (sides[3] > 0))
+                {
+                    if (sides[0] + sides[2] != sides[1] + sides[3])
+                    {
+                        throw new ArgumentException();
+                    }
+
+                }
+            }
+        }
+
+    }
+    public class Tetragon : IComparable<Tetragon>
+    {
+        public Sides Sides { get; set; }
+        public int P { get; set; }
+
+        public void Input()
+        {
+            var sides = new Sides();
+            Console.WriteLine("Введите стороны 4-угольника");
+            for (int i = 0; i < sides.sides.Length; i++)
+            {
+                sides[i] = int.Parse(Console.ReadLine());
+                P += sides[i];
+            }
+            Sides = sides;
+        }
+        public int this[int index]
+        {
+
+            get { return Sides[index]; }
+            set
+            {
+                if (value <= 0) throw new ArgumentException();
+                Sides[index] = value;
+                if (Sides[0] != 0 && Sides[1] != 0 && Sides[2] != 0)
+                {
+                    if ((Sides[0] > Sides[2] + Sides[1]) || (Sides[1] > Sides[2] + Sides[0]) || (Sides[2] > Sides[0] + Sides[1]))
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+
+            }
+        }
+        public int CompareTo(Tetragon? tetragon)
+        {
+            if (tetragon == null) throw new ArgumentNullException("Четырехугольник задан некорректно");
+
+            return P.CompareTo(tetragon.P);
+        }
+        public void PrintTetragon(Tetragon tetragon)
+        {
+            Console.WriteLine("{0}", tetragon.P);
+            Console.WriteLine();
+        }
+    }
+
+    class TetragonArray
+    {
+        public int TetragonCount { get; set; }
+        public Tetragon[] tetragons { get; set; }
+        public TetragonArray(int tetragonCount)
+        {
+            TetragonCount = tetragonCount;
+            tetragons = new Tetragon[TetragonCount];
+        }
+        public void TetragonInput()
+        {
+            for (int i = 0; i < TetragonCount; i++)
+            {
+                tetragons[i] = new Tetragon();
+                tetragons[i].Input();
+            }
+        }
+    }
+    class Comparator : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var tetragon1 = (Tetragon)x;
+            var tetragon2 = (Tetragon)y;
+            if ((x == null) || (y == null))
+                throw new ArgumentException("Стороны заданы некорректно");
+            return
+                tetragon1.P.CompareTo(tetragon2.P);
+        }
+    }
+    static class ArrayExtensions
+    {
+        public static void Swap(this Array array, int i, int j)
+        {
+            object obj = array.GetValue(i);
+            array.SetValue(array.GetValue(j), i);
+            array.SetValue(obj, j);
+        }
+
+        //public static void SortIComparable(this Array tetragons)
+        //{
+        //    for (int i = tetragons.Length - 1; i >= 0; i--)
+        //    {
+        //        for (int j = 1; j <= i; j++)
+        //        {
+        //            var element1 = (IComparable)tetragons.GetValue(i);
+        //            var element0 = tetragons.GetValue(j - 1);
+        //            if (element1.CompareTo(element0) < 0)
+        //            {
+        //                tetragons.Swap(j - 1, j);
+
+        //            }
+        //        }
+
+        //    }
+        //}
+    }
+    class Program
+    {
+        public static void SortIComparer(Tetragon[] tetragons, IComparer comparer)
+        {
+            for (int i = tetragons.Length - 1; i > 0; i--)
+                for (int j = 1; j <= i; j++)
+                {
+                    object element1 = tetragons.GetValue(j - 1);
+                    object element2 = tetragons.GetValue(j);
+                    if (comparer.Compare(element1, element2) > 0)
+                    {
+                        object temporary = tetragons.GetValue(j);
+                        tetragons.SetValue(tetragons.GetValue(j - 1), j);
+                        tetragons.SetValue(temporary, j - 1);
+                    }
+                }
+        }
+        static void Main()
+        {
+            Tetragon tetragon = new Tetragon();
+            Console.WriteLine("Введите кол-во четырехугольников: ");
+            TetragonArray x = new TetragonArray(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Ввод сторон");
+            x.TetragonInput();
+            x.tetragons[1].Sides[3] = 150;
+            Console.Write("4 сторона 2-го четырехугольника: ");
+            Console.WriteLine(x.tetragons[1].Sides[3]);
+            //Console.WriteLine(tetragons[]);
+            //x.tetragons.SortIComparable();
+            //Console.WriteLine("Отсортированные SortIComparable периметры:");
+            //foreach (var tetr in x.tetragons)
+            //{
+            //    tetr.PrintTetragon(tetr);
+            //}
+
+            SortIComparer(x.tetragons, new Comparator());
+            Console.WriteLine("Отсортированные SortIComparer периметры:");
+            foreach (var tetr in x.tetragons)
+            {
+                tetr.PrintTetragon(tetr);
+            }
+
+        }
+    }
+}
